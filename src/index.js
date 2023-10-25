dc0c315fb88e891c8c83f8ff3c58078b0b3cbc4e
vaj573 <jhonedw573@gmail.com>
2023-10-25 01:39:09 -0500

    Comentarios en js para la clave de api

---
+++ src/index.js
@@ -0,0 +1,5 @@
+//Clave de la API: 4037711053de8efe03398288380ebc9e
+
+// Token de acceso de lectura a la API: 
+
+// eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0MDM3NzExMDUzZGU4ZWZlMDMzOTgyODgzODBlYmM5ZSIsInN1YiI6IjY1MzhhMmNhMGZiMTdmMDBmZWIwZTg4OSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.UxYhSmIFVn6tD_sMBEEmpjwh8WjvI_HmrbH0Ps04Qsc
