d3fc1547283f91cee5be7a136c562d664502f928
cuestionario <31359170+cuestionario@users.noreply.github.com>
2023-10-27 23:57:14 -0500

    initial comit

--- src/index.js
+++ src/index.js
@@ -3,3 +3,16 @@
 // Token de acceso de lectura a la API: 
 
 // eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0MDM3NzExMDUzZGU4ZWZlMDMzOTgyODgzODBlYmM5ZSIsInN1YiI6IjY1MzhhMmNhMGZiMTdmMDBmZWIwZTg4OSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.UxYhSmIFVn6tD_sMBEEmpjwh8WjvI_HmrbH0Ps04Qsc
+
+document.addEventListener("DOMContentLoaded", function () {
+    const navLinks = document.querySelectorAll("nav ul li a");
+
+    navLinks.forEach((link) => {
+        link.addEventListener("click", (event) => {
+            navLinks.forEach((item) => {
+                item.classList.remove("active");
+            });
+            event.target.classList.add("active");
+        });
+    });
+});
