c4a10357ca56fd7df5acfeb6ca37a524d34c096a
Sebatianbotia <122814675+Sebatianbotia@users.noreply.github.com>
2023-10-30 12:18:01 -0500

    Footer and modal parcial finish

--- src/index.js
+++ src/index.js
@@ -3,48 +3,5 @@
 // Token de acceso de lectura a la API: 
 
 // eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0MDM3NzExMDUzZGU4ZWZlMDMzOTgyODgzODBlYmM5ZSIsInN1YiI6IjY1MzhhMmNhMGZiMTdmMDBmZWIwZTg4OSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.UxYhSmIFVn6tD_sMBEEmpjwh8WjvI_HmrbH0Ps04Qsc
-const refs = {
-    openModal: document.querySelector('.open-modal-team'),
-    closeModal: document.querySelector('.close-modal-team'),
-    teamBackdrop: document.querySelector('.backdrop-modal'),
-    teamModal: document.getElementsByClassName('team__modal'),
-  };
-  
-  refs.openModal.addEventListener('click', openModalTeam);
-  refs.closeModal.addEventListener('click', closeModalTeam);
-  
-  function openModalTeam(event) {
-    console.log("se ha clickeado")
-    refs.teamBackdrop.classList.remove('team__backdrop--hidden');
-    document.addEventListener('keydown', onEscapeClose);
-    document.addEventListener('click', onBackdropClose);
-    refs.teamModal[0].classList.add('openModalAnimationTeam');
-  }
-  
-  function closeModalTeam(event) {
-    refs.teamModal[0].classList.remove('closeModalAnimationTeam');
-    refs.teamBackdrop.classList.add('team__backdrop--hidden');
-    document.removeEventListener('keydown', onEscapeClose);
-    document.body.style.overflow = '';
-  }
-  
-  function onEscapeClose(event) {
-    if (event.code === 'Escape') {
-      refs.teamModal[0].classList.remove('openModalAnimationTeam');
-      refs.teamModal[0].classList.add('closeModalAnimationTeam');
-      setTimeout(() => {
-        closeModalTeam();
-      }, 400);
-      closeModalTeam();
-    }
-  }
-  
-  function onBackdropClose(event) {
-    if (event.target === refs.teamBackdrop) {
-      refs.teamModal[0].classList.remove('openModalAnimationTeam');
-      refs.teamModal[0].classList.add('closeModalAnimationTeam');
-      setTimeout(() => {
-        closeModalTeam();
-      }, 400);
-    }
-  }
+import * as openModal from './js/Modal';
+import * as teamModal from './js/Modal';
