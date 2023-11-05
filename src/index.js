10893773907a60c875d37a92907aa81f38a91a34
Sebatianbotia <122814675+Sebatianbotia@users.noreply.github.com>
2023-11-05 09:53:06 -0500

    partial modal config

--- src/index.js
+++ src/index.js
@@ -7,3 +7,5 @@ import * as openModal from './js/Modal';
 import * as teamModal from './js/Modal';
 import "./js/popular";
 import "./js/buscar";
+import "./js/movie-modal";
+
---
+++ src/js/movie-modal.js
@@ -0,0 +1,6 @@
+const movieApiKey = "4037711053de8efe03398288380ebc9e";
+
+document.querySelector('.movie').addEventListener('click', function() {
+    console.log("se ha clickeado el div");
+  });
+
